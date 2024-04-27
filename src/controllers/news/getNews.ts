f41489727ca1910a58e804d09dbbcfcdaff1a7d5
Tien25012003 <21520112@gm.uit.edu.vn>
2024-04-27 19:01:54 +0700

    Feat - Paging News

--- src/controllers/news/getNews.ts
+++ src/controllers/news/getNews.ts
@@ -3,12 +3,30 @@ import { NewCategoryModel } from "../../models/NewCategory";
 import { NewsModel } from "../../models/News";
 import ErrorUtils from "../../utils/constant/Error";
 export const getNews = async (req: Request, res: Response) => {
-  const { id } = req.query;
+  const { id, page = 1, size = 7 } = req.query;
   try {
+    const category = await NewCategoryModel.findById(id);
+    const totalCount = category?.listNews.length || 0;
     await NewCategoryModel.findById(id)
-      .populate({ path: "listNews", model: NewsModel })
-      .then((category) => res.status(200).json(category?.listNews));
+      .populate({
+        path: "listNews",
+        model: NewsModel,
+        options: {
+          sort: { createdAt: -1 },
+          skip: (+page - 1) * +size,
+          //skip: 0,
+          limit: +size,
+        },
+      })
+      .then((category) => {
+        res.send({
+          code: 200,
+          data: category?.listNews,
+          totalCount: totalCount,
+        });
+      });
   } catch (e) {
+    console.log(e);
     res.send(ErrorUtils.get("SERVER_ERROR"));
   }
 };
