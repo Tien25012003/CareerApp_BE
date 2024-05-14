bf4ce55560acfdf89f59b77b86c4a84d01bc9aed
Tien25012003 <21520112@gm.uit.edu.vn>
2024-05-14 20:46:58 +0700

    Feat - Update OCR

--- src/controllers/ocr/utils/convertTextToArray.ts
+++ src/controllers/ocr/utils/convertTextToArray.ts
@@ -1,11 +1,9 @@
 const SUBJECTS = ["Toán", "Vật lí", "Hóa học", "Sinh học", "Tin học", "Địa lí"];
 export const convertTextToArray = (text: string) => {
-  // Split the text by line breaks
   const lines = text.split("\n");
-  const subjects: Array<{ [key in any]: string }> = [];
+  const subjects: string[] = [];
   for (const line of lines) {
-    const [subject, score] = line.split(":").map((item) => item.trim());
-    subjects.push({ [subject.toString()]: score.toString() } as never);
+    subjects.push(line?.trim());
   }
   return subjects;
 };
