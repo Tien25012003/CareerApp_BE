62387ebc580b95a9d7d650b56bc13ec6f69eddc5
Tien25012003 <21520112@gm.uit.edu.vn>
2024-05-19 10:57:32 +0700

    Fix bug - Remove Redundancy

--- src/routes/ocrRouter.ts
+++ src/routes/ocrRouter.ts
@@ -1,9 +1,7 @@
 import { Router } from "express";
-import { convertImageToText } from "../controllers/ocr/convertImageToText";
 import { ocrGemini } from "../controllers/OCR/ocrGemini";
 import upload from "../middlewares/upload";
 
 const ocrRouter = Router();
-ocrRouter.post("/convertImageToText", convertImageToText);
 ocrRouter.post("/ocrGemini", upload.single("file"), ocrGemini);
 export default ocrRouter;
