69a88f8bfd5f42499784c6d13ec7fbdbf3e68fb3
TanKhang <doank3442@gmail.com>
2024-11-16 21:16:59 +0700

    refactor/create-news

--- src/controllers/news/getAllNews.ts
+++ src/controllers/news/getAllNews.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import { NewsModel } from "../../models/News";
 import ErrorUtils from "../../utils/constant/Error";
-import { TResponse } from "../../utils/types/meta";
+import { TPagingParams, TResponseWithPagination } from "../../utils/types/meta";
 type Image = {
   longImage: string;
   shortImage: string;
@@ -15,18 +15,42 @@ type NewsItem = {
   type: "BREAKING" | string; // Assuming other types could be added
   image: Image;
 };
+type TParams = {
+  search: string;
+  type: string;
+};
 export const getAllNews = async (
-  req: Request<any, any, any, any>,
-  res: Response<TResponse<NewsItem[]>>
+  req: Request<any, any, any, any, TParams & Partial<TPagingParams>>,
+  res: Response<TResponseWithPagination<NewsItem[]>>
 ) => {
+  const { search, type, page = 1, size = 10 } = req.query;
+  let query: any = {};
+  if (search) {
+    const searchPattern = new RegExp(search, "i");
+    query["$or"] = [{ title: searchPattern }, { content: searchPattern }];
+  }
+  if (type) {
+    query["type"] = type;
+  }
+  console.log(query);
   try {
-    const news = await NewsModel.find({});
+    const news = await NewsModel.find(query)
+      .skip((page - 1) * size)
+      .limit(size)
+      .exec();
+    console.log(news);
+    const countNews = await NewsModel.countDocuments(query);
     //console.log(news);
     res.send({
       code: 200,
       //@ts-expect-error no check
       data: news,
-      message: "",
+      pagination: {
+        size: +size,
+        page: +(page - 1),
+        totalCounts: countNews,
+        totalPages: Math.ceil(countNews / +size),
+      },
     });
   } catch (error) {
     res.send(ErrorUtils.get("SERVER_ERROR"));
